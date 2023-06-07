598d962eb409492f2d16d4d7e9ba6457035991a3
adfwer233 <ba123698745@126.com>
2023-06-07 22:27:00 +0800

    feat: forwarding json message

--- src/main/java/com/thss/androidbackend/websocket/ChatWebsocketHandler.java
+++ src/main/java/com/thss/androidbackend/websocket/ChatWebsocketHandler.java
@@ -15,6 +15,33 @@ import java.util.concurrent.CopyOnWriteArrayList;
 
 @RequiredArgsConstructor
 public class ChatWebsocketHandler extends TextWebSocketHandler {
+
+    class MessageToClient {
+        private String senderId;
+        private String message;
+
+        public MessageToClient(String senderId, String message) {
+            this.senderId = senderId;
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public String getSenderId() {
+            return senderId;
+        }
+
+        public void setSenderId(String senderId) {
+            this.senderId = senderId;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
+    }
+
     private static final List<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
 
 //    private final RedisTemplate<String, String> websocketSessionMapping;
@@ -35,22 +62,33 @@ public class ChatWebsocketHandler extends TextWebSocketHandler {
             JsonNode jsonNode = objectMapper.readTree(json);
             System.out.println(jsonNode);
 
-            String senderId = jsonNode.get("senderId").asText();
-            String receiverId = jsonNode.get("receiverId").asText();
-            String messageContent = jsonNode.get("message").asText();
-
-            if (Boolean.FALSE.equals(websocketSessionMapping.containsKey(senderId))) {
+            String operation = jsonNode.get("operation").asText();
+            if (operation.equals("register")) {
+                String senderId = jsonNode.get("senderId").asText();
                 websocketSessionMapping.put(senderId, session.getId());
-            }
+            } else if (operation.equals("send")) {
+                String senderId = jsonNode.get("senderId").asText();
+                String receiverId = jsonNode.get("receiverId").asText();
+                String messageContent = jsonNode.get("message").asText();
 
-            // save message to database
+                if (Boolean.FALSE.equals(websocketSessionMapping.containsKey(senderId))) {
+                    websocketSessionMapping.put(senderId, session.getId());
+                }
 
-            if (Boolean.TRUE.equals(websocketSessionMapping.containsKey(receiverId))) {
-                // send message to receiver if websocket connect on
-                String receiverSessionId = websocketSessionMapping.get(receiverId);
-                for (WebSocketSession sess: sessions) {
-                    if (sess.getId().equals(receiverSessionId)) {
-                        sess.sendMessage(new TextMessage(messageContent));
+                // save message to database
+
+                if (Boolean.TRUE.equals(websocketSessionMapping.containsKey(receiverId))) {
+                    // send message to receiver if websocket connect on
+                    String receiverSessionId = websocketSessionMapping.get(receiverId);
+                    for (WebSocketSession sess : sessions) {
+                        if (sess.getId().equals(receiverSessionId)) {
+                            // create json object
+                            MessageToClient messageToClient = new MessageToClient(senderId, messageContent);
+                            ObjectMapper mapper = new ObjectMapper();
+                            String jsonString = mapper.writerWithDefaultPrettyPrinter()
+                                    .writeValueAsString(messageToClient);
+                            sess.sendMessage(new TextMessage(jsonString));
+                        }
                     }
                 }
             }

922523b5ddfa58175a7d7478cec0385ed9b6e7fc
adfwer233 <ba123698745@126.com>
2023-06-07 21:41:26 +0800

    feat: add websocket forwarding message

--- src/main/java/com/thss/androidbackend/config/WebsocketConfig.java
+++ src/main/java/com/thss/androidbackend/config/WebsocketConfig.java
@@ -3,6 +3,7 @@ package com.thss.androidbackend.config;
 import com.thss.androidbackend.websocket.ChatWebsocketHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
--- src/main/java/com/thss/androidbackend/websocket/ChatWebsocketHandler.java
+++ src/main/java/com/thss/androidbackend/websocket/ChatWebsocketHandler.java
@@ -1,15 +1,24 @@
 package com.thss.androidbackend.websocket;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.JsonNode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.socket.*;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
+@RequiredArgsConstructor
 public class ChatWebsocketHandler extends TextWebSocketHandler {
     private static final List<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
 
-
+//    private final RedisTemplate<String, String> websocketSessionMapping;
+    private final HashMap<String, String> websocketSessionMapping = new HashMap<>();
     @Override
     public void afterConnectionEstablished(WebSocketSession session) {
         System.out.println("test");
@@ -18,10 +27,39 @@ public class ChatWebsocketHandler extends TextWebSocketHandler {
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) {
-        System.out.println(message);
-//        for (WebSocketSession s : sessions) {
-//            s.sendMessage(message);
-//        }
+        System.out.println(message.getPayload());
+
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            String json = message.getPayload();
+            JsonNode jsonNode = objectMapper.readTree(json);
+            System.out.println(jsonNode);
+
+            String senderId = jsonNode.get("senderId").asText();
+            String receiverId = jsonNode.get("receiverId").asText();
+            String messageContent = jsonNode.get("message").asText();
+
+            if (Boolean.FALSE.equals(websocketSessionMapping.containsKey(senderId))) {
+                websocketSessionMapping.put(senderId, session.getId());
+            }
+
+            // save message to database
+
+            if (Boolean.TRUE.equals(websocketSessionMapping.containsKey(receiverId))) {
+                // send message to receiver if websocket connect on
+                String receiverSessionId = websocketSessionMapping.get(receiverId);
+                for (WebSocketSession sess: sessions) {
+                    if (sess.getId().equals(receiverSessionId)) {
+                        sess.sendMessage(new TextMessage(messageContent));
+                    }
+                }
+            }
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
     }
 
     @Override

5a2a9b2f3ee87b05f38f34700b2080d92d4a61b5
deevem <Deevem@126.com>
2023-06-10 20:12:18 +0800

    Global gitignored

--- src/main/java/com/thss/androidbackend/Global.java
+++ src/main/java/com/thss/androidbackend/Global.java
@@ -1,5 +1,5 @@
 package com.thss.androidbackend;
 
 public class Global {
-    public static final String HOST = "http://183.172.178.189:8080";
+    public static final String HOST = "http://192.168.1.101:8080";
 }
